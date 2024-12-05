d049727ff1b5cb51e0fe91aa6c7aafff41d90cd9
Souran <abc@gmail.com>
2024-12-05 01:42:46 -0500

    finish Fcfs algo and metric calculation

--- interrupts.c
+++ interrupts.c
@@ -1,6 +1,31 @@
+/**
+ * Implementation of a Scheduler Simulato for SYSC 4001 Assignment 3
+ * Dec 5th, 2024
+ * Jordan Grewal, 101287828
+ * Hadi Srour, 101200666
+ */
 #include "interrupts.h"
+// Global variables
+long int simTime = 0;
+PCB readyQue[MAX_PROCESSESS];
+int readyQueSize = 0;
+PCB waitingQue[MAX_PROCESSESS];
+int waitingQueSize = 0;
+int processTerminated = 0;
+int number_of_process;
+// int newArr = 1;
+PCB running;
+// PCB processess[MAX_PROCESSESS];
+PCBandArrival newArr[MAX_PROCESSESS];
+PCBandArrival memory_update[MAX_PROCESSESS];
+int memory_update_size=0;
 
-void initMemory(MemoryPartition partitions[])
+MemoryPartition partitions[TOTAL_PARTITIONS];
+
+processMatrics metrics[MAX_PROCESSESS];
+int index_of_pid = 0;
+
+void initMemory()
 {
     // Initialize the memory
     unsigned int size[TOTAL_PARTITIONS] = {40, 25, 15, 10, 8, 2};
@@ -11,24 +36,27 @@ void initMemory(MemoryPartition partitions[])
         partitions[i].occupiedBy = FREE;
     }
 }
-void read_input_data(const char *filename, PCB process[], int *process_counter)
+int read_input_data(const char *filename)
 {
+    int process_counter = 0;
     FILE *file = fopen(filename, "r");
     if (file)
     {
 
         while (fscanf(file, "%d, %u, %u, %u, %u, %u\n",
-                      &process[*process_counter].PID,
-                      &process[*process_counter].memory_size,
-                      &process[*process_counter].arrival_time,
-                      &process[*process_counter].CPU_time,
-                      &process[*process_counter].IO_frequency,
-                      &process[*process_counter].IO_duration) == 6)
+                      &newArr[process_counter].pcb.PID,
+                      &newArr[process_counter].pcb.memory_size,
+                      &newArr[process_counter].pcb.arrival_time,
+                      &newArr[process_counter].pcb.CPU_time,
+                      &newArr[process_counter].pcb.IO_frequency,
+                      &newArr[process_counter].pcb.IO_duration) == 6)
         {
-            process[*process_counter].remaining_CPU_time = process[0].CPU_time;
-            process[*process_counter].partition_number = FREE;
-            process[*process_counter].state = NEW;
-            (*process_counter)++;
+            newArr[process_counter].pcb.remaining_CPU_time = newArr[0].pcb.CPU_time;
+            newArr[process_counter].pcb.remaining_IO_frequency = newArr[0].pcb.IO_frequency;
+            newArr[process_counter].pcb.remaining_IO_duration = newArr[0].pcb.IO_duration;
+            newArr[process_counter].pcb.partition_number = FREE;
+
+            process_counter++;
         }
         fclose(file);
     }
@@ -36,8 +64,9 @@ void read_input_data(const char *filename, PCB process[], int *process_counter)
     {
         perror("Failed to load input file.\n");
     }
+    return process_counter;
 }
-void printMemory(MemoryPartition partitions[])
+void printMemory()
 {
     // Print the memory
 
@@ -52,37 +81,36 @@ void printMemory(MemoryPartition partitions[])
                partitions[i].occupiedBy);
     }
 }
-void printProcess(PCB *process, int *process_counter)
+void printProcess(PCBandArrival process[], int *process_counter)
 {
     // Print the process
 
     printf("process Table:\n");
-    printf("PID     | Size (MB) | Arrival Time | Total CPU Time | I/O Freq | I/O Duration | Partition Number | State\n");
-    printf("-----------------------------------------------------------------------------------------------------------\n");
+    printf("PID     | Size (MB) | Arrival Time | Total CPU Time | I/O Freq  | I/O Duration | Partition Number\n");
+    printf("-------------------------------------------------------------------------------------------------\n");
     for (unsigned int i = 0; i < *process_counter; i++)
     {
-        printf("%7u | %7u | %12u | %10u | %10u | %10u | %10d | %10d\n",
-               process[i].PID,
-               process[i].memory_size,
-               process[i].arrival_time,
-               process[i].CPU_time,
-               process[i].IO_frequency,
-               process[i].IO_duration,
-               process[i].partition_number,
-               process[i].state);
+        printf("%7u | %8u | %12u | %15u | %10u | %13u | %16d\n",
+               process[i].pcb.PID,
+               process[i].pcb.memory_size,
+               process[i].pcb.arrival_time,
+               process[i].pcb.CPU_time,
+               process[i].pcb.IO_frequency,
+               process[i].pcb.IO_duration,
+               process[i].pcb.partition_number);
     }
 }
 // allocate memory to process
-int allocate_memory(PCB *process, MemoryPartition *partition)
+int allocate_memory(PCBandArrival *process)
 {
     for (int i = 0; i < TOTAL_PARTITIONS - 1; i++)
     {
-        if (process->memory_size <= partition[i].size)
+        if (process->pcb.memory_size <= partitions[i].size)
         {
-            if (process->memory_size >= partition[i+1].size && partition[i].occupiedBy == FREE)
+            if (process->pcb.memory_size >= partitions[i + 1].size && partitions[i].occupiedBy == FREE)
             {
-                partition[i].occupiedBy = process->PID;
-                process->partition_number = i;
+                partitions[i].occupiedBy = process->pcb.PID;
+                process->pcb.partition_number = i+1;
                 return 1;
             }
         }
@@ -95,102 +123,288 @@ int deallocate_memory(PCB *process)
 
     return 0;
 }
+void printMetrics()
+{
+    printf("------------------------------------------------------------------------------------------\n");
+    printf("PID  | Arrival Time | End Time | Turnaround Time | Wait Time | Burst Time | Mean Wait Time\n");
+    printf("------------------------------------------------------------------------------------------\n");
+    for (int i = 0; i < number_of_process; i++)
+    {
+        printf("%-4d | %-12d | %-8d | %-15d | %-10d | %-10d | %-14.2lf\n",
+               metrics[i].pid,
+               metrics[i].arrivalTime,
+               metrics[i].endTime,
+               metrics[i].turnaroundTime,
+               metrics[i].waitingTime,
+               metrics[i].burstTime,
+               metrics[i].meanWaitTime);
+        /* code */
+    }
+    printf("------------------------------------------------------------------------------------------\n");
+}
+void initMetrics()
+{
+    for (int i = 0; i < number_of_process; i++)
+    {
+        metrics[i].pid = newArr[i].pcb.PID;
+        metrics[i].arrivalTime = 0;
+        metrics[i].endTime = 0;
+        metrics[i].turnaroundTime = 0;
+        metrics[i].waitingTime = 0;
+        metrics[i].burstTime = 0;
+        metrics[i].meanWaitTime = 0.0;
+
+        /* code */
+    }
+}
+
+int findMetric(int id)
+{
+    for (int i = 0; i < number_of_process; i++)
+    {
+        if (metrics[i].pid == id)
+        {
+            return i;
+        }
+        /* code */
+    }
+    exit(0);
+}
 // Function to log state transitions
-void log_transition(FILE *file, int time, int pid, const char *old_state, const char *new_state) {
+void log_transition(FILE *file, int time, int pid, const char *old_state, const char *new_state)
+{
     fprintf(file, "| %-17d | %-3d | %-9s | %-10s |\n", time, pid, old_state, new_state);
 }
-void create_execution_file(PCB *process){
+
+void printStateChange(int id, char msg[])
+{
+    printf("%ldms ID-%d: %s\n", simTime, id, msg);
+}
+void simulation(PCBandArrival processes[], int counter, FILE *file)
+{
+
+    _Bool cpu_busy = 0; // cpu free
+    int process_count = counter;
+    int arrivalSize = 0;
+
+
+    while (processTerminated < counter)
+    {
+        if (arrivalSize == 0)
+        {
+            for (int i = 0; i < process_count; i++)
+            {
+
+                if (newArr[i].pcb.arrival_time <= simTime)
+                {
+                    readyQue[readyQueSize] = processes[i].pcb;
+
+                    printStateChange(readyQue[readyQueSize].PID, "NEW      ---> READY");
+                    log_transition(file, simTime, readyQue[readyQueSize].PID, "NEW", "READY");
+                    
+                    // allocate_memory(&processes[i]);
+                    // printProcess(processes,&number_of_process);
+                    if (i < process_count - 1)
+                    {
+                        for (int a = i; a < process_count - 1; a++)
+                        {
+                            newArr[a] = newArr[a + 1];
+                        }
+                        // dec size of newArr, dec i so as we check the new value we shifted into i
+                        process_count--;
+                        i--;
+                    }
+                    // case 2: i is at end
+                    else
+                    {
+                        process_count--;
+                        // check if empty
+                        if (process_count == 0)
+                        {
+                            arrivalSize = 1;
+                        }
+                    }
+                    index_of_pid = findMetric(readyQue[readyQueSize].PID);
+                    metrics[index_of_pid].arrivalTime = simTime;
+                    readyQueSize++;
+                }
+            }
+        }
+
+        if (cpu_busy == 0) // if cpu is free
+        {
+            if (readyQueSize > 0)
+            {
+                running = readyQue[0];
+
+                for (int i = 0; i < readyQueSize - 1; i++) // if there is more than 1 process in ready que
+                {
+                    readyQue[i] = readyQue[i + 1];
+                }
+                readyQueSize--;
+                printStateChange(running.PID, "READY      ---> RUNNING");
+                log_transition(file, simTime, running.PID, "READY", "RUNNING");
+
+                cpu_busy = 1;
+            }
+        }
+        simTime++;
+        // if cpu is busy
+        if (cpu_busy)
+        {
+            running.remaining_CPU_time--;
+            running.remaining_IO_frequency--;
+            // check if process is done
+            if (running.remaining_CPU_time <= 0)
+            {
+                // update metric
+                index_of_pid = findMetric(running.PID);
+                metrics[index_of_pid].burstTime++;
+                metrics[index_of_pid].endTime = simTime;
+                metrics[index_of_pid].turnaroundTime = simTime - metrics[index_of_pid].arrivalTime;
+                metrics[index_of_pid].meanWaitTime = metrics[index_of_pid].waitingTime / metrics[index_of_pid].burstTime;
+
+                // log
+                printStateChange(running.PID, "RUNNING      ---> TERMINATE");
+                log_transition(file, simTime, running.PID, "RUNNING", "TERMINATE");
+                
+
+                // Free CPU and increment finished process
+                cpu_busy = 0;
+                processTerminated++;
+            }
+            // if process is not done, it will go to IO
+            else if (running.remaining_IO_frequency <= 0)
+            {
+                // update metric
+                index_of_pid = findMetric(running.PID);
+                metrics[index_of_pid].burstTime++;
+
+                printStateChange(running.PID, "RUNNING      ---> WAITING");
+                log_transition(file, simTime, running.PID, "RUNNING", "WAITING");
+                running.remaining_IO_frequency = running.IO_frequency;
+                waitingQue[waitingQueSize] = running;
+                waitingQueSize++;
+                cpu_busy = 0;
+            }
+        }
+        // process to do IO
+        for (int i = 0; i < waitingQueSize; i++)
+        {
+            //  printf("IO Remaining: %u\n",waitingQue[i].remaining_IO_duration);
+            // check if it is done with IO
+            if (waitingQue[i].remaining_IO_duration == 0)
+            {
+                waitingQue[i].remaining_IO_duration = waitingQue[i].IO_duration;
+                readyQue[readyQueSize] = waitingQue[i];
+                readyQueSize++;
+                printStateChange(waitingQue[i].PID, "WAITING      ---> READY");
+                log_transition(file, simTime, waitingQue[i].PID, "WAITING", "READY");
+                // case 1: i is at start or middle
+                if (i < waitingQueSize - 1)
+                {
+                    for (int a = i; a < waitingQueSize - 1; a++)
+                    {
+                        waitingQue[a] = waitingQue[a + 1];
+                    }
+                    // dec size of newArr, dec i so as we check the new value we shifted into i
+                    waitingQueSize--;
+                }
+                // case 2: i is at end
+                else
+                {
+                    waitingQueSize--;
+                }
+                /* code */
+            }
+            waitingQue[i].remaining_IO_duration--;
+        }
+        // for process waiting in ready queue
+        for (int i = 0; i < readyQueSize; i++)
+        {
+            index_of_pid = findMetric(readyQue[i].PID);
+            metrics[index_of_pid].waitingTime++;
+            /* code */
+        }
+    }
+}
+void create_mem_partition_file(){
+    FILE *file = fopen("memory_partition.txt", "w");
+     // Print table header to the file
+    fprintf(file, "+------------------------------------------------------------------------------------------+\n");
+    fprintf(file, "| Time of Event | Memory Used | Partitions State  | Total Free Memory | Usable Free Memory |\n");
+    fprintf(file, "+------------------------------------------------------------------------------------------+\n");
+
+    // Print table footer to the file
+    fprintf(file, "+------------------------------------------------------------------------------------------+\n");
+
+    // Close the file
+    fclose(file);
+
+    printf("Data successfully written to memory_events.txt.\n");
+
+}
+void create_execution_file(PCBandArrival processes[], int counter)
+{
     FILE *file = fopen("execution.txt", "w");
 
     // Print header
-    fprintf(file, "+------------------------------------------------+\n");
+    fprintf(file, "+--------------------------------------------------+\n");
     fprintf(file, "|Time of Transition | PID | Old State | New State |\n");
-    fprintf(file, "+------------------------------------------------+\n");
+    fprintf(file, "+--------------------------------------------------+\n");
 
-    //log 
-    fprintf(file, "| %-17d | %-3d | %-9d | %-10d |\n", process->arrival_time, process->PID, process->state, process->state+1);
-    
+    // Run simulation and log each transition
+    simulation(processes, counter, file);
 
     // Print footer
-    fprintf(file, "+------------------------------------------------+\n");
+    fprintf(file, "+--------------------------------------------------+\n");
 
     fclose(file);
 }
-// FCFS Scheduler
-void fcfs_scheduler(PCB processes[], int *process_count, const char* output_file) {
-    FILE* file = fopen(output_file, "w");
-    if (!file) {
-        perror("Error opening output file");
-        exit(EXIT_FAILURE);
+void printFinalStat(double wait_time, double tnt_time){
+    for (int i = 0; i < number_of_process; i++)
+    {  
+        wait_time+= metrics[i].waitingTime;
+        tnt_time+= metrics[i].turnaroundTime;
+
+        /* code */
     }
+    wait_time = wait_time/number_of_process;
+    tnt_time = tnt_time/number_of_process;
 
-    fprintf(file, "Time of Transition | PID | Old State | New State |\n");
-    fprintf(file, "+------------------------------------------------+\n");
-
-    int current_time = 0;
-
-    for (int i = 0; i < *process_count; i++) {
-        PCB* process = &processes[i];
-
-        // Transition NEW -> READY       
-        fprintf(file, "| %16d | %3d |       NEW |     READY |\n", current_time, process->PID);
-        // strcpy(process->state, "READY");
-        process->state = READY;
-
-
-        // Transition READY -> RUNNING
-        fprintf(file, "| %16d | %3d |     READY |   RUNNING |\n", current_time, process->PID);
-        // strcpy(process->state, "RUNNING");
-        process->state = RUNNING;
-
-        while (process->remaining_CPU_time > 0) {
-            // Simulate I/O if required
-            if (process->IO_frequency > 0 && process->remaining_CPU_time > process->IO_frequency) {
-                // Transition RUNNING -> WAITING
-                current_time += process->IO_frequency;
-                fprintf(file, "| %16d | %3d |   RUNNING |   WAITING |\n", current_time, process->PID);
-                // strcpy(process->state, "WAITING");
-                process->state = WAITING;
-                process->remaining_CPU_time -= process->IO_frequency;
-
-                // Transition WAITING -> READY
-                current_time+= process->IO_duration;
-                fprintf(file, "| %16d | %3d |   WAITING |     READY |\n", current_time, process->PID);
-                // strcpy(process->state, "READY");
-                process->state = READY;
-                // current_time+= process->IO_duration;
-
-                // Transition READY -> RUNNING
-                fprintf(file, "| %16d | %3d |     READY |   RUNNING |\n", current_time, process->PID);
-                process->state= RUNNING;
-            }
-
-            // Process runs for 1ms
-            if(process->remaining_CPU_time < process->IO_frequency){
-                process->remaining_CPU_time--;
-                current_time++;
-            }
-            
-        }
-
-        // Transition RUNNING -> TERMINATE
-        fprintf(file, "| %16d | %3d |   RUNNING | TERMINATE |\n", current_time, process->PID);
-        // strcpy(process->state, "TERMINATE");
-        process->state = TERMINATE;
-    }
-
-    fclose(file);
+    printf("\n Average Process Wait Time: %0.2lfms\n Average Turnaround Time: %0.2lfms\n Average Throughput: %0.2lf\n ",
+    wait_time,tnt_time,(100/tnt_time));
+    
 }
 int main()
 {
-    MemoryPartition partitions[TOTAL_PARTITIONS];
-    PCB processess[MAX_PROCESSESS];
-    // initMemory(partitions);
-    // printMemory(partitions);
+    _Bool no_process_left = 0;
+    _Bool cpu_busy = 0;
+    double avrg_wait_time=0.0;
+    double avrg_tnt_time = 0.0;
+    // PCBandArrival *temp = newArr;
+    initMemory();
+    printMemory();
     // first come first serve
-    int process_count = 0;
-    read_input_data("input_data.txt", processess, &process_count);
-    fcfs_scheduler(processess,&process_count,"execution.txt");
-    // printProcess(process, &process_count);
+
+    // load all process from .txt file into newArr array
+    number_of_process = read_input_data("input_data_2.txt");
+    initMetrics();
+    printProcess(newArr, &number_of_process);
+    printf("+-----------------------------------------------------------------+\n");
+    printf("------------------- Start of simulation ---------------------------\n");
+    printf("+-----------------------------------------------------------------+\n");
+    create_execution_file(newArr, number_of_process);
+    printf("+-----------------------------------------------------------------+\n");
+    printf("--------------------- Simulation Over -----------------------------\n");
+    // printProcess(newArr, &number_of_process);
+    printf("+-----------------------------------------------------------------+\n");
+    printf("\n------------------- Printing of Metrics ---------------------------\n");
+    printMetrics();
+    printf("\n------------------------ Final Stats -----------------------------\n");
+    printFinalStat(avrg_wait_time,avrg_tnt_time);
+
+  
+    
     return 1;
 }
--- interrupts.h
+++ interrupts.h
@@ -1,8 +1,8 @@
 /**
- * Interrupt simulator header file for SYSC 4001 Assignment 2
- * November 1st, 2024
- * 
- * 
+ * Implementation of a Scheduler Simulato for SYSC 4001 Assignment 3
+ * Dec 5th, 2024
+ * Jordan Grewal, 101287828
+ * Hadi Srour, 101200666
  */
 
 #include <stdio.h>
@@ -15,143 +15,47 @@
 #define INTERRUPTS_H
 
 #define TOTAL_PARTITIONS 6
-#define FREE -1 // define free space 
+#define FREE -1 // define free space
 #define MAX_PROCESSESS 100
-enum ProcessState{
-    NEW,
-    READY,
-    RUNNING,
-    WAITING,
-    TERMINATE
 
-};
-typedef struct {
+typedef struct
+{
     unsigned int partitionNumber; // PID
-    unsigned int size; //Size of the partition
-    int occupiedBy; //PID of the occupied
+    unsigned int size;            // Size of the partition
+    int occupiedBy;               // PID of the occupied
 } MemoryPartition;
 
-typedef struct {
-    char program_name[20];
-    unsigned int size;
-} ExternalFile;
-
-typedef struct {
+typedef struct
+{
     int PID;
-    unsigned int CPU_time;
-    unsigned int IO_duration;
-    unsigned int IO_frequency;
-    unsigned int remaining_CPU_time;  
-    unsigned int memory_size;
-    unsigned int arrival_time;
-    int partition_number; 
-    enum ProcessState state;
+    unsigned int CPU_time;               // Total CPU Time
+    unsigned int IO_duration;            // Time to run I/O
+    unsigned int IO_frequency;           // Process to perform I/O
+    unsigned int remaining_CPU_time;     // How much time left in CPU for process
+    unsigned int remaining_IO_duration;  // Time left in I/O
+    unsigned int remaining_IO_frequency; // Time left for process to perfrom I/O
+    unsigned int memory_size;            // Memory occupied
+    unsigned int arrival_time;           // Arrival time
+    int partition_number;                // To determine the occupied memory
 } PCB;
 
+typedef struct
+{
+    PCB pcb; //
+    unsigned int arrival_time;
 
+} PCBandArrival;
 
+typedef struct
+{
+    int pid;            // process id
+    int endTime;        // when process end
+    int arrivalTime;    // when process was added to ready queue
+    int turnaroundTime; // process turnaround time
+    int waitingTime;    // Totol time the process waited in the ready queue
+    int burstTime;
+    double meanWaitTime;
 
-/**
- * Function to simulate a fork() system call
- * 
- */
-void forkSyscall(int parent_pid, int dur);
-
-/**
- * Function to simulate exec syscall
- */
-void execSyscall(int pid, const char* filename, int dur);
-
-/**
- * Logs system status after fork or exec syscalls.
- * Stores information in system_status.txt
- */
-void logSystemStatus();
-
-/**
- * Function to setup pcb tables, memory partitions, external files, etc.
- * Runs at start of program
- */
-void initalizeSystem();
-
-/**
- * This function reads the vector_table.txt file, storing it's contents
- * in the global array vectorTable.
- */
-void read_vector_table();
-
-/**
- * This function sets up the output file, overwriting any content
- * at the start of the program, ensuring that only new information is stored
- * in a "fresh" file.
- */
-void setupOutputFile();
-
-/**
- * This function is just a random number generator for use throughout the program
- * generates random number from 1-3
- * 
- * @return random number from 1 to 3
- */
-int rand_num();
-
-/**
- * This function stores the output/simulation text
- * into the execution.txt file.
- * Uses the outputText and outputCounter global variables.
- * resets outputText and outputCounter when complete.
- */
-void store_output();
-
-/**
- * This function procedurally "prints" out each line of execution into an array of strings
- * (outputText). if the outputText array is full, it calls store_output() to store the contents
- * of outputText. increases the timer based on duration of each call.
- * 
- * @param duration the duration of the call
- * @param message the message/description of the call
- */
-void printer(int duration, char* message);
-
-/**
- * This function is the ISR routine for SYSCALLs.
- * It takes in the information provided by the trace file,
- * "calling" the appropriate SYSCALL with information from the vectorTable.
- * 
- * @param num the position of the vector table provided by the trace file.
- * @param dur the duration of the event provided by the trace file
- */
-void SYSCALL_ISR(int num, int dur, int type, char* program);
-
-/**
- * This function is the ISR routine for END_IO.
- * It works the same as SYSCALL_ISR(), but with appropriate activities
- * for the END_IO routine.
- * 
- * @param num the position of the vector table provided by the trace file.
- * @param dur the duration of the event provided by the trace file
- */
-void ENDIO_ISR(int num, int dur);
-
-/**
- * This function parses information taken from the trace file, 
- * calling ISR routines if needed, and printing other lines directly using the 
- * printer() function. 
- * 
- * @param p1 The Message of an event from trace file
- * @param p2 If present, the position of a ISR Routine from trace file
- * @param p3 The duration of an event from trace file
- */
-void executeLine(char *p1, char *p2, char *p3);
-
-/**
- * main loop of program
- * Opens and reads information from the trace file.
- * Goes line by line, parsing information and calling executeLine() to execute each line as
- * they are read.
- * 
- * @param fileName The file name of the trace text file.
- */
-void executionLoop(char *fileName);
+} processMatrics;
 
 #endif
